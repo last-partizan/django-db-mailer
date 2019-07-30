0550789e93b1995084dd88eb564c7d66cea49f04
Sergey Tereschenko <serg.partizan@gmail.com>
2019-07-30 14:38:03 +0300

    don't use html editor by default

--- dbmail/fields.py
+++ dbmail/fields.py
@@ -1,22 +1,21 @@
 # -*- encoding: utf-8 -*-
-
+from django.conf import settings
 from django.db import models
 
 from dbmail import app_installed
 
+USE_HTML_EDITOR = getattr(settings, "DBMAIL_USE_HTML_EDITOR", False)
 HTMLField = models.TextField
 DataTextField = models.TextField
 
-if app_installed('tinymce'):
+if USE_HTML_EDITOR and app_installed('tinymce'):
     try:
         from tinymce.models import HTMLField
-
     except ImportError:
         pass
 
-if app_installed('ckeditor'):
+if USE_HTML_EDITOR and app_installed('ckeditor'):
     try:
         from ckeditor.fields import RichTextField as HTMLField
-
     except ImportError:
         pass
